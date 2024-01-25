88ca7a2c261ef5ee0615d4340c4e4c4150933666
YouriProgrammer <97059905+YOURIxYOURI@users.noreply.github.com>
2024-01-25 21:23:20 +0100

    v.1

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,97 @@
-﻿namespace commsTCP;
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace commsTCP;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    TcpListener server;
+    TcpClient client;
+    NetworkStream serverStream;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        InitializeTCP();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    void InitializeTCP()
+    {
+        server = new TcpListener(IPAddress.Any, 12345);
+        server.Start();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        Task.Run(() => StartServer());
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        client = new TcpClient();
+        client.Connect("127.0.0.1", 54321);
+        serverStream = client.GetStream();
+
+        Task.Run(() => StartClient());
+    }
+
+    async Task StartServer()
+    {
+        while (true)
+        {
+            TcpClient client = await server.AcceptTcpClientAsync();
+            await Task.Run(() => HandleClient(client));
+        }
+    }
+
+    [Obsolete]
+    async Task StartClient()
+    {
+        while (true)
+        {
+            byte[] data = new byte[256];
+            int bytes = await serverStream.ReadAsync(data, 0, data.Length);
+            string message = Encoding.ASCII.GetString(data, 0, bytes);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                ChatMessagesStack.Children.Add(new Label { Text = "Otrzymano: " + message });
+            });
+        }
+    }
+
+    [Obsolete]
+    async Task HandleClient(TcpClient client)
+    {
+        using (NetworkStream stream = client.GetStream())
+        {
+            while (true)
+            {
+                byte[] data = new byte[256];
+                int bytes = await stream.ReadAsync(data, 0, data.Length);
+                string message = Encoding.ASCII.GetString(data, 0, bytes);
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    ChatMessagesStack.Children.Add(new Label { Text = "Otrzymano: " + message });
+                });
+            }
+        }
+    }
+
+    [Obsolete]
+    async void SendMessage_Clicked(object sender, EventArgs e)
+    {
+        string message = MessageEntry.Text;
+
+        if (!string.IsNullOrWhiteSpace(message))
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            await serverStream.WriteAsync(data, 0, data.Length);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                ChatMessagesStack.Children.Add(new Label { Text = "Wysłano: " + message });
+            });
+        }
+        MessageEntry.Text = string.Empty;
+    }
 }
+
+
 
