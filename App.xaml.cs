05980d3100eabb00d9ac3cb5c2e970f1ac389f9f
YouriProgrammer <97059905+YOURIxYOURI@users.noreply.github.com>
2024-01-25 20:18:22 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace commsTCP;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace commsTCP;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
